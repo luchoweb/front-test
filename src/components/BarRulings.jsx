4c43b42ee15fba77ff82591720cf29af1c6e92b5
Lucho Web <webfacilco@gmail.com>
2022-09-10 11:03:30 -0500

    Card Ruling Styles progress

---
+++ src/components/BarRulings.jsx
@@ -0,0 +1,7 @@
+const BarRulings = ({ votes }) => {
+  return (
+    <></>
+  )
+}
+
+export default BarRulings;
--- src/components/CardRuling.jsx
+++ src/components/CardRuling.jsx
@@ -1,32 +1,45 @@
 import moment from "moment/moment";
 
 import "../styles/components/CardRuling.scss";
-
-const picturePath = '/assets/img';
+import BarRulings from "./BarRulings";
 
 const CardRuling = ({ data }) => {
+  const isPositiveVotes = data.votes.positive >= data.votes.negative;
+  const truncateLimit = 16;
+  const truncateDots = data.name.length > truncateLimit ? '...' : '';
+
   return (
     <div
       className="card-ruling"
       style={{
-        backgroundImage: `url(${picturePath}/${data.picture})`
+        backgroundImage: `url(/assets/img/${data.picture})`
       }}
     >
-      <div className="card-ruling__head">
-        <p className="card-ruling__head-title">
-          { data.name } 
-        </p>
+      <div className="card-ruling__container">
+        <div className="card-ruling__head">
+          <p className="card-ruling__head-title">
+            <span className={`card-ruling__head-title-icon card-ruling__head-title-icon--bg-${ isPositiveVotes ? 'green' : 'yellow' }`}>
+              <img src={`/assets/img/thumbs-${ isPositiveVotes ? 'up' : 'down' }.svg`} />
+            </span>
+            <span>
+              {`${ data.name.substr(0, truncateLimit) }${ truncateDots }`}
+            </span>
+          </p>
+        </div>
+
         <p className="card-ruling__description">
           { data.description }
         </p>
+
+        <p className="card-ruling__last-updated">
+          { moment(data.lastUpdated).fromNow() } in
+          <span className="card-ruling__category">
+            { data.category }
+          </span>
+        </p>
       </div>
 
-      <p className="card-ruling__last-updated">
-        { moment(data.lastUpdated).fromNow() } in
-        <span className="card-ruling__category">
-          { data.category }
-        </span>
-      </p>
+      <BarRulings votes={data.votes} />
     </div>
   )
 }
